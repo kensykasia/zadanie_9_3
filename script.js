65b55f70e1f3ecd2c53b21740174ef49e8471e68
Katarzyna Kensy <zbierek@gmail.com>
2018-04-19 14:28:55 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first ap';
+var dinosaur = 'Triceratops';
+dinosaur.toUpperCase();
+text = text.replace('Velociraptor', dinosaur);
+console.log(text.length/2);
